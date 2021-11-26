f2ab29589768737523f47867fc5ee00821abb01d
Astridcc01 <93380210+Astridcc01@users.noreply.github.com>
2021-11-26 13:00:00 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Cliente{
+    public static void main(String args[]){
+        Nombre nombre = new Nombre();
+        cliente.setNombre("Indra");
+        cliente.setCiudad("Madrid");
+        cliente.setPresupuesto("10000");
+        
+        System.out.println(cliente.getNombre());
+        System.out.println(cliente.getCiudad());
+        System.out.println(cliente.getPresupuesto());
+    }
+}
+
+public class Cliente2{
+    public static void main(String args[]){
+        Nombre nombre = new Nombre();
+        cliente.setNombre("Indra");
+        cliente.setCiudad("Madrid");
+        cliente.setPresupuesto("10000");
+        
+        System.out.println(cliente.getNombre());
+        System.out.println(cliente.getCiudad());
+        System.out.println(cliente.getPresupuesto());
+    }
+}
